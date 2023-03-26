f47ef81e45f07b9e4465af272a8c57da9760ca75
RathierS <rathier.sylia@gmail.com>
2023-03-26 01:00:07 +0100

    Autocompletion et carte 2

---
+++ ressources/js/autocomplete.js
@@ -0,0 +1,252 @@
+//Récupère les div qui servent à l'autocompletion
+    var liste_ville = document.getElementById("autocompletion");
+    var auto2 = document.getElementById("autocompletionArrivee");
+
+    //Fonction pour afficher le nom des ville dans la div
+    //5 villes affichees (voir la requete sql)
+    //tableau -> nom des ville, divComplete -> indique l'autocompletion pour ville de départ ou d'arrivée
+    function afficheVilles(tableau, divComplete) {
+        //console.log(tableau);
+        videVilles(divComplete);
+        for (let i = 0; i < tableau.length; i++) {
+            let ville = document.createElement("p");
+            ville.classList.add("ma-classe");
+            ville.textContent = tableau[i].nom_comm;
+            divComplete.appendChild(ville);
+        }
+        divComplete.style.borderWidth = `${tableau.length > 0 ? 1 : 0}px`
+    }
+
+    //Méthode pour vider la div d'autocompletion
+    //divComplete -> indique l'autocompletion pour ville de départ ou d'arrivée
+    function videVilles(divComplete) {
+        let child = divComplete.lastElementChild;
+        while (child) {
+            divComplete.removeChild(child);
+            child = divComplete.lastElementChild;
+        }
+
+    }
+
+    //Pas utiliser mais permet l'affichage d'un gif
+    function actionDebut() {
+        loading.style.visibility = "visible";
+    }
+    //Pas utiliser mais permet d'enlever l'affichage d'un gif
+    function actionFin() {
+        loading.style.visibility = "hidden";
+    }
+
+    //Fais une requête pour récupérer les données et appelle le callback mis en paramètre
+    function requeteAJAX(stringVille, callback, action_debut, action_fin) {
+        let url = "controleurFrontal.php?action=autocompletion&controleur=noeudCommune&nomCommuneDepart=" + encodeURIComponent(stringVille);
+        let requete = new XMLHttpRequest();
+        requete.open("GET", url, true);
+        //requete.addEventListener("loadstart", action_debut)
+        requete.addEventListener("load", function () {
+            callback(requete);
+            //action_fin();
+        });
+        requete.send();
+    }
+
+    //Appelle requeteAJAX avec ces parametres
+    function maRequeteAJAX(string) {
+        //mettre tout le temps la première lettre en majuscule au cas ou il est en minuscule et mettre les autres lettres en minuscules
+        string = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+        requeteAJAX(string, callback_4, actionDebut, actionFin);
+    }
+
+    //Met la requete en Json et appelle l'affichage des villes en fonction de l'input
+    function callback_4(req) {
+        //tab = tab.map(e => e.name);
+        //console.log(tab);
+        document.addEventListener('input', function(event) {
+            let tab = JSON.parse(req.responseText);
+            if (event.target.id === 'nomCommuneDepart_id') {
+                afficheVilles(tab, liste_ville);
+            }
+            if (event.target.id === 'nomCommuneArrivee_id') {
+                afficheVilles(tab, auto2);
+            }
+        });
+
+
+    }
+
+    //recupère le input de départ
+    var nomCommuneDepart = document.getElementById("nomCommuneDepart_id");
+    //Apelle maRequete lorsque l'input est utilisé
+    //Le vide ville ne fonctionne pas ici
+    nomCommuneDepart.addEventListener("input", function () {
+        //console.log("chargement terminé !!!");
+        let stringVille = nomCommuneDepart.value;
+        console.log(stringVille);
+        console.log(stringVille.length);
+
+        if(stringVille.length === 0 ){
+            console.log('avant');
+            console.log(liste_ville);
+            videVilles(liste_ville);
+            console.log("après");
+            console.log(liste_ville);
+
+
+        }
+        else {
+            console.log("requete");
+            maRequeteAJAX(stringVille)
+        }
+
+
+    });
+
+    //recupère le input d'arrivée
+    var nomCommuneArrivee = document.getElementById("nomCommuneArrivee_id");
+    //Apelle maRequete lorsque l'input est utilisé
+    //Le vide ville ne fonctionne pas ici
+
+    nomCommuneArrivee.addEventListener("input", function () {
+        //console.log("chargement terminé !!!");
+        let stringVille = nomCommuneArrivee.value;
+        console.log(stringVille);
+        console.log(stringVille.length);
+        maRequeteAJAX(stringVille);
+        if (stringVille.length <= 0) {
+            videVilles(auto2);
+            auto2.style.borderWidth = "0px";
+
+        }
+
+    });
+
+    //vide la liste des villes dans l'autocompletion lorsque l'on click sur une ville
+document.addEventListener("click", function(event){
+    if(event.target.className === "ma-classe") {
+        var targetElement = event.target;
+        nomCommuneDepart.value = targetElement.textContent;
+        videVilles(liste_ville);
+    }
+});
+
+//--------------------------------------------------------------
+//Le reste permet l'utilisation des flèches pour descendre
+// et monter et la touche entrer pour selectionner une ville
+
+//Met à jour l'index qui permet de savoir où on se situ dans la liste
+function updateSelectedIndex(l, selectedIndex) {
+    for (let i = 0; i < l.length; i++) {
+        l[i].classList.remove("selected");
+        l[i].style.background = "white"
+    }
+    // Ajoute la sélection sur l'élément correspondant à l'indice sélectionné
+    if (selectedIndex >= 0 && selectedIndex < l.length) {
+        l[selectedIndex].classList.add("selected");
+        l[selectedIndex].style.background = "grey";
+
+    }
+}
+
+
+document.addEventListener('input', function(event) {
+
+    if (event.target.id === 'nomCommuneDepart_id') {
+        let selectedIndex1 = -1; // Le premier élément sélectionné est -1 (aucun)
+
+        const liste = liste_ville.getElementsByTagName("p");
+        // Fonction pour mettre à jour la sélection visuelle de la liste des suggestions
+        updateSelectedIndex(liste,selectedIndex1);
+
+        // Gestionnaire d'événements pour la touche haut
+        nomCommuneDepart.addEventListener("keydown", function(event) {
+            if (event.keyCode === 38) { // 38 est le code de la touche haut
+                console.log("38");
+                event.preventDefault(); // Empêche le défilement de la page
+                if(selectedIndex1 -1 >= 0 ) {
+                    selectedIndex1--;
+                    updateSelectedIndex(liste, selectedIndex1);
+                }
+            }
+        });
+
+        // Gestionnaire d'événements pour la touche bas
+        nomCommuneDepart.addEventListener("keydown", function(event) {
+            if (event.keyCode === 40) { // 40 est le code de la touche bas
+                event.preventDefault(); // Empêche le défilement de la page
+                if(selectedIndex1 +1 < liste.length) {
+                    selectedIndex1++;
+                    updateSelectedIndex(liste, selectedIndex1);
+                }
+            }
+        });
+
+        // Gestionnaire d'événements pour la touche Entrée
+        nomCommuneDepart.addEventListener("keydown", function(event) {
+            if (event.keyCode === 13) { // 13 est le code de la touche Entrée
+                event.preventDefault(); // Empêche le défilement de la page
+                if (selectedIndex1 >= 0 && selectedIndex1 < liste.length) {
+                    // Sélectionne l'élément correspondant à l'indice sélectionné
+                    const selectedSuggestion = liste[selectedIndex1];
+                    console.log(selectedSuggestion);
+                    // Met à jour le champ texte avec la suggestion sélectionnée
+                    nomCommuneDepart.value = selectedSuggestion.textContent;
+                    // Efface la liste des suggestions
+                    videVilles(liste_ville);
+                    // Réinitialise l'indice de sélection
+                    selectedIndex1 = -1;
+                }
+            }
+        });
+    }
+
+
+
+
+    if (event.target.id === 'nomCommuneArrivee_id') {
+        let selectedIndex2 = -1;
+        const liste2 = auto2.getElementsByTagName("p");
+        updateSelectedIndex(liste2, selectedIndex2);
+
+        nomCommuneArrivee.addEventListener("keydown", function(event) {
+            if (event.keyCode === 38) {
+                event.preventDefault();
+                if(selectedIndex2 -1 >= 0 ) {
+                    selectedIndex2--;
+                    updateSelectedIndex(liste2, selectedIndex2);
+                }
+            }
+        });
+
+        nomCommuneArrivee.addEventListener("keydown", function(event) {
+            if (event.keyCode === 40 ) {
+                event.preventDefault();
+                if(selectedIndex2 +1 < liste2.length){
+                    selectedIndex2++;
+                    updateSelectedIndex(liste2, selectedIndex2);
+                }
+
+            }
+        });
+
+        nomCommuneArrivee.addEventListener("keydown", function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                if (selectedIndex2 >= 0 && selectedIndex2 < liste2.length) {
+                    const selectedSuggestion = liste2[selectedIndex2];
+                    nomCommuneArrivee.value = selectedSuggestion.textContent;
+                    videVilles(auto2);
+                    selectedIndex2 = -1;
+                }
+            }
+        });
+    }
+});
+
+
+
+
+
+
+
+//});
+
---
+++ ressources/js/map.js
@@ -0,0 +1,70 @@
+
+// Initialisation de la carte
+var mymap = L.map('mapid').setView([46.485935, 2.553603], 6);
+
+// Ajout de la carte OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+//Initialisation du tableau de coordonnées
+var coords = [];
+
+// Tracer un itinéraire à partir d'un tableau de coordonnées (c'est un exemple)
+var routeCoords = [[51.505, -0.09], [51.507, -0.08], [51.509, -0.10]];
+var route = L.polyline(routeCoords, {color: 'red'}).addTo(mymap);
+
+//Fonction qui pour afficher les point sur la carte.
+//prend en paramètre un tableau de coordonnees de la ville
+function affichePoint(tableau){
+    let latitude = tableau[0];
+    let longitude = tableau[1];
+    tabVille = [longitude, latitude];
+    coords.push(tabVille);
+    console.log(coords);
+    JSON.stringify(coords);
+    //coords = [[51.509, -0.10], [43.59917865, 3.894125217]];
+    for (var i = 0; i < coords.length; i++) {
+        L.marker(coords[i]).addTo(mymap);
+    }
+
+}
+
+//Recupère les données (coordonnées) grâce à l'url et appelle la fonction affichePoint grâce aux coordonnées
+//Methide GET
+function maRequete(string){
+    let url = "controleurFrontal.php?action=donneesCarte&controleur=noeudCommune&nomCommuneDepart=" + encodeURIComponent(string);
+    let requete = new XMLHttpRequest();
+    requete.open("GET", url, true);
+    //requete.addEventListener("loadstart", action_debut)
+    requete.addEventListener("load", function () {
+        let tab = JSON.parse(requete.responseText);
+        //console.log(JSON.parse(tab[0].st_asgeojson));
+        tableau = JSON.parse(tab[0].st_asgeojson);
+        //console.log(tableau.coordinates);
+        affichePoint(tableau.coordinates);
+    });
+    requete.send(null);
+}
+
+//Une fois le calcule effectuer la ville de départ et la ville d'arrivé sont placés dans
+//des balise span et puis récupérer ici pour avoir le nom des ville.
+var nomDepart = document.getElementById("nomDepart");
+var nomArrivee = document.getElementById("nomArrivee");
+
+//Attend que la page soit charcé pour effectuer les requètes
+document.addEventListener("DOMContentLoaded",  function() {
+    console.log("La section spécifique de la page a fini de se charger !");
+    if(nomDepart && nomArrivee ) {
+        let stringDepart = nomDepart.textContent;
+        let stringArrivee = nomArrivee.textContent;
+        //Requète sur la ville de départ
+        maRequete(stringArrivee);
+        //Requète sur la ville d'arrivée
+        maRequete(stringDepart);
+    }
+
+
+});
+
