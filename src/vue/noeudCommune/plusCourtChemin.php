5a13582ff20c0af882de5549eae08e79d8666d20
Kim Harribaud <kim.harribaud@etu.umontpellier.fr>
2023-04-09 19:03:24 +0200

    commit page calcul css

--- src/vue/noeudCommune/plusCourtChemin.php
+++ src/vue/noeudCommune/plusCourtChemin.php
@@ -5,6 +5,7 @@
                 <h1 class="titre">Calculez votre chemin </h1>
                 <div class="separateur"></div>
             </div>
+            <div class="input">
             <div class="localisation">
                 <input class="InputAddOn-field" type="text" value="" placeholder="Ville de départ ou position actuelle" name="nomCommuneDepart" id="nomCommuneDepart_id" required>
                 <i id="localisation" class="fa-solid fa-location-crosshairs"></i>
@@ -15,10 +16,9 @@
 
             <div class="localisation">
                 <input class="InputAddOn-field" type="text" value="" placeholder="Destination souhaitée" name="nomCommuneArrivee" id="nomCommuneArrivee_id" required>
-                <i id="localisation2" class="fa-solid fa-location-crosshairs"></i>
             </div>
             <div id="autocompletionArrivee"></div>
-
+            </div>
             <input type="hidden" name="XDEBUG_TRIGGER">
             <input class="button-calcul" type="submit" value="C'est parti !" />
 
